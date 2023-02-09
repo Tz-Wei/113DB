fb2662cf6a3f298e5b0c844ed2ab723f9fcae907
Tz-Wei <weiweihung16@gmail.com>
2023-02-08 21:40:47 -0800

    Initial Commit

---
+++ 113DB/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  113DB
+//
+//  Created by Wei Wei Hung on 2/8/23.
+//  Copyright © 2023 CS32. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
